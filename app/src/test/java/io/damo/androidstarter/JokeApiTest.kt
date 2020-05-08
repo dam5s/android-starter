dcfd4ba6d3117cde4730557894f198dd783ab592
Damien Le Berrigaud <damien@itsdamo.com>
2020-05-07 18:30:58 -0600

    Add Api tests.

---
+++ app/src/test/java/io/damo/androidstarter/JokeApiTest.kt
@@ -0,0 +1,47 @@
+package io.damo.androidstarter
+
+import io.damo.androidstarter.JokeApi.JokeJson
+import io.damo.androidstarter.support.Success
+import io.damo.androidstarter.support.baseUrl
+import io.damo.androidstarter.support.enqueue
+import io.damo.androidstarter.support.startMockServer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Test
+
+class JokeApiTest {
+
+    private val mockServer = startMockServer()
+    private val api = JokeApi(mockServer.baseUrl())
+
+    @After
+    fun teardown() {
+        mockServer.shutdown()
+    }
+
+    @Test
+    fun testGetRandomJoke() {
+        val jokeContent =
+            "Chuck Norris doesn't need a keyboard he tells the computer to write something and it does."
+
+        mockServer.enqueue(
+            body = """{
+                "type": "success", 
+                "value": { 
+                    "id": 605, 
+                    "joke": "$jokeContent", 
+                    "categories": ["nerdy"] 
+                } 
+            }"""
+        )
+
+        val result = api.getRandomJoke()
+
+        val expectedJoke = JokeJson(605, jokeContent)
+        assertThat(result).isEqualTo(Success(expectedJoke))
+
+        val recordedRequest = mockServer.takeRequest()
+        assertThat(recordedRequest.method).isEqualTo("GET")
+        assertThat(recordedRequest.path).isEqualTo("/jokes/random")
+    }
+}
